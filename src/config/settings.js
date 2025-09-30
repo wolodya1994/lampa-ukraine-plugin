945da90648ed3bbe8a22cf473fdba54ab30e6980
wolodya1994 <80901460+wolodya1994@users.noreply.github.com>
2025-10-01 01:35:44 +0200

    Налаштування

---
+++ src/config/settings.js
@@ -0,0 +1,12 @@
+export const config = {
+    name: 'Ukrainian Sources',
+    version: '1.0.0',
+    author: 'Your Name',
+    description: 'Плагін з українськими балансерами'
+};
+
+export const defaults = {
+    selectedProvider: 'animeon',
+    autoPlay: false,
+    quality: 'HD'
+};
